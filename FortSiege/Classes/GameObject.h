433eb358f6f73c96446ed3c5da26164b46823626
Kel Cecil <kelcecil@praisechaos.com>
2011-04-04 14:32:59 -0400

    Added GameObject.h and GameObject.m

---
+++ FortSiege/Classes/GameObject.h
@@ -0,0 +1,51 @@
+//
+//  GameObject.h
+//  FortSiege
+//
+//  Created by Kel Cecil on 4/4/11.
+//  Copyright 2011 WVU. All rights reserved.
+//
+//  Not using @synthesize to reinforce the syntax of the new language.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface Position : NSObject {
+@private
+    int x;
+    int y;
+}
+
+@property int x;
+@property int y;
+
+-(Position*) init;
+-(Position*) initWithCoordinatesX:(int)X andY:(int)Y;
+
+@end
+
+@interface Velocity : NSObject {
+@private
+    int deltaX;
+    int deltaY;
+}
+
+@property int deltaX;
+@property int deltaY;
+
+-(Velocity*) initWithVelocityX:(int)inputDeltaX andY:(int)inputDeltaY;
+-(Velocity*) init;
+@end
+
+@interface GameObject : NSObject {
+@private
+    Position* position;
+    Velocity* velocity;
+}
+
+@property (retain) Position* position;
+@property (retain) Velocity* velocity;
+
+@end
+
---
+++ FortSiege/Classes/GameObject.m
@@ -0,0 +1,85 @@
+//
+//  GameObject.m
+//  FortSiege
+//
+//  Created by Kel Cecil on 4/4/11.
+//  Copyright 2011 WVU. All rights reserved.
+//
+
+#import "GameObject.h"
+
+
+@implementation GameObject
+
+@synthesize position;
+@synthesize velocity;
+
+-(GameObject*) init {
+    
+    if (self == [super init]) {
+        position = [[Position alloc] init];
+        velocity = [[Velocity alloc] init];
+    }
+    return self;
+}
+
+-(void) dealloc {
+    [self.position release];
+    [self.velocity release];
+    [super dealloc];
+}
+
+@end
+
+@implementation Position
+@synthesize x;
+@synthesize y;
+
+-(Position*) init {
+    if (self == [super init]) {
+        self.x = 0;
+        self.y = 0;
+    }
+    return self;
+}
+
+-(Position*) initWithCoordinatesX:(int)X andY:(int)Y{
+    
+    if (self == [super init]) {
+        self.x = X;
+        self.y = Y;
+    }
+    return self;
+}
+
+-(void) dealloc {
+    [super dealloc];
+}
+
+@end
+
+@implementation Velocity
+@synthesize deltaX;
+@synthesize deltaY;
+
+-(Velocity*) init {
+    if (self == [super init]) {
+        self.deltaX = 0;
+        self.deltaY = 0;
+    }
+    return self;
+}
+
+-(Velocity*) initWithVelocityX:(int)inputDeltaX andY:(int)inputDeltaY {
+    if (self == [super init]) {
+        self.deltaX = inputDeltaX;
+        self.deltaY = inputDeltaY; 
+    }
+    return self;
+}
+
+-(void) dealloc {
+    [super dealloc];
+}
+
+@end
