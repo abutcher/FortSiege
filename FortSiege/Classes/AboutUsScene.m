377e36850cafc182669e531680967667e3271f61
Kel Cecil <kelcecil@praisechaos.com>
2011-04-11 12:59:41 -0400

    Classes/AboutUsScene.h

--- FortSiege/Classes/AboutUsScene.m
+++ FortSiege/Classes/AboutUsScene.m
@@ -23,11 +23,26 @@
     
 }
 
+-(void) BackToHome
+{
+    [[CCDirector sharedDirector] replaceScene:[MainMenuScene node]];
+}
+
 -(id) init{
     
     if ( ( self = [super init] )) {
         
+        CGSize winSize = [[CCDirector sharedDirector] winSize];
         
+        NSLog(@"init a la fortsiege");
+        
+        CCLabelTTF *_back = [[CCLabelTTF labelWithString:@"Back" fontName:@"Arial" fontSize:32.0] retain];
+        CCMenuItem *_backItem = [CCMenuItemLabel itemWithLabel:_back target:self selector:@selector(BackToHome)];
+        _backItem.position = ccp(winSize.width/10,50);
+        
+        CCMenu *aboutMenu = [CCMenu menuWithItems:_backItem,nil];
+        aboutMenu.position = CGPointZero;
+        [self addChild:aboutMenu];        
         
     }
     
