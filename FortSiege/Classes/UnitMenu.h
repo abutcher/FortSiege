baa00cb9bcc621c1b3d44ebb45841a678cbd9484
Daniel Sloan <DaltonKelison@Pegasus.local>
2011-04-11 01:24:09 -0400

    First commit, getting my menuing stuff in

---
+++ FortSiege/Classes/UnitMenu.h
@@ -0,0 +1,16 @@
+//
+//  UnitMenu.h
+//  FortSiege
+//
+//  Created by Daniel Sloan on 4/11/11.
+//  Copyright 2011 WVU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UnitMenu : what {
+    
+}
+
+@end
---
+++ FortSiege/Classes/UnitMenu.m
@@ -0,0 +1,14 @@
+//
+//  UnitMenu.m
+//  FortSiege
+//
+//  Created by Daniel Sloan on 4/11/11.
+//  Copyright 2011 WVU. All rights reserved.
+//
+
+#import "UnitMenu.h"
+
+
+@implementation UnitMenu
+
+@end
