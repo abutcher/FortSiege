8d776a58d42054b1cf826009077c97d668439a4e
Kel Cecil <kelcecil@praisechaos.com>
2011-04-15 14:53:42 -0400

    added as much backend code for other classes as I could. Crashes on missing sprites.

--- FortSiege/Classes/KingArthur.m
+++ FortSiege/Classes/KingArthur.m
@@ -11,4 +11,74 @@
 
 @implementation KingArthur
 
+
+-(void) prepareFrames{
+    NSMutableArray* walkingAnimationFrames = [NSMutableArray array];
+    NSMutableArray* standingAnimationFrames = [NSMutableArray array];
+    NSMutableArray* attackingAnimationFrames = [NSMutableArray array];
+    
+    for (int i = 1; i <= 6; ++i) {
+        [walkingAnimationFrames addObject: [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName: [NSString stringWithFormat:@"kingar_walk_0%d.png", i]]];
+    }
+    
+    self.walkAnimation = [CCAnimation animationWithFrames:walkingAnimationFrames delay:0.1f];
+    self.walkAction = [CCRepeatForever actionWithAction: [CCAnimate actionWithAnimation:self.walkAnimation restoreOriginalFrame:YES]];
+    
+    for (int i = 1; i <=1; ++i) {
+        [standingAnimationFrames addObject: [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:[NSString stringWithFormat:@"kingar_stand_0%d.png",i]]];
+    }
+    
+    self.standAnimation = [CCAnimation animationWithFrames:standingAnimationFrames delay:0.1f];
+    self.standAction = [CCRepeatForever actionWithAction:[CCAnimate actionWithAnimation:self.standAnimation restoreOriginalFrame:YES]];
+    
+    for (int i = 1; i<=3; ++i) {
+        [attackingAnimationFrames addObject: [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:[NSString stringWithFormat:@"kingar_attack_0%d.png",i]]];
+    }
+    
+    self.attackAnimation = [CCAnimation animationWithFrames:attackingAnimationFrames delay:0.1f];
+    self.attackAction = [CCRepeatForever actionWithAction:[CCAnimate actionWithAnimation:self.attackAnimation restoreOriginalFrame:YES]];
+}
+
+-(id) init {
+    
+    if ( ( self = [super init] )) {
+        
+        [self prepareFrames];
+        
+        self.character = [CCSprite spriteWithSpriteFrameName:@"kingar_walk_01.png"];
+        [self runWalkAction];
+        
+    }
+    
+    return self;
+}
+
+-(id) summonWithParameters: (int)x y: (int)y state: (State) state facing: (Facing) facing {
+    if (( self = [super init])) {
+        
+        [self prepareFrames];
+        
+        self.state = state;
+        self.facing = facing;
+        
+        if (self.state == WALKING) {
+            self.character = [CCSprite spriteWithSpriteFrameName:@"kingar_walk_01.png"];
+            [self runWalkAction];
+        }
+        else if (self.state == STANDING) {
+            self.character = [CCSprite spriteWithSpriteFrameName:@"kingar_stand_01.png"];
+            [self runStandAction];
+        }
+        
+        self.position.x = x;
+        self.position.y = y;
+        self.character.position = ccp(x, y);
+        
+    }
+    return self;
+}
+
+-(void) updateObject:(ccTime)dt {
+    [super updateObject:dt];
+}
 @end
